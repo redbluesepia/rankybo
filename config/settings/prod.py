291754a59c4a9165c02364187ae8dc560e30ad94
redblues <rankystock@gmail.com>
2021-12-22 00:44:10 +0900

    postgresql 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,3 +3,14 @@ from .base import *
 ALLOWED_HOSTS = ['54.180.207.198']
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rankybo',
+        'USER': 'redblues',
+        'PASSWORD': 'i^mredsp6l',
+        'HOST': '54.180.207.198',
+        'PORT': '5432',
+    }
+}
